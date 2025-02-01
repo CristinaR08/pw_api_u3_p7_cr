14198fdf92c29ba8eff9a9129f0785b8916f1885
CristinaR08 <cris.04082009@hotmail.com>
2025-02-01 00:45:07 +0000

    Taller 29: Verbos

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ The application is now runnable using `java -jar target/quarkus-app/quarkus-run.
 If you want to build an _über-jar_, execute the following command:
 
 ```shell script
-./mvnw package -Dquarkus.package.jar.type=uber-jar
+./mvnw package "-Dquarkus.package.jar.type=uber-jar"
 ```
 
 The application, packaged as an _über-jar_, is now runnable using `java -jar target/*-runner.jar`.
@@ -49,7 +49,7 @@ Or, if you don't have GraalVM installed, you can run the native executable build
 ./mvnw package -Dnative -Dquarkus.native.container-build=true
 ```
 
-You can then execute your native executable with: `./target/pw_api_u3_p7_cr-1.0.0-SNAPSHOT-runner`
+You can then execute your native executable with: `./target/pw_api_u3_p7_cr-1.0.0-SNAPSHOT-runner.jar`
 
 If you want to learn more about building native executables, please consult <https://quarkus.io/guides/maven-tooling>.
 
--- src/main/java/uce/edu/web/api/controller/PersonaController.java
+++ src/main/java/uce/edu/web/api/controller/PersonaController.java
@@ -1,10 +1,12 @@
 package uce.edu.web.api.controller;
 
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Response;
 import uce.edu.web.api.service.IPersonaService;
 import uce.edu.web.api.service.to.PersonaTo;
 
@@ -14,24 +16,39 @@ public class PersonaController {
     @Inject
     private IPersonaService personaService;
 
+    @Path("/buscar")
+    @GET
+    public PersonaTo buscarPorId() {
+        Integer id = 1;
+        return this.personaService.buscarPorId(id);
+        //return Response.ok(this.personaService.buscarPorId(id)).build();
+    }
+
+    @POST
+    @Path("/guardar")
     public void guardar(PersonaTo persona) {
         this.personaService.guardar(persona);
     }
 
+    @PUT
+    @Path("/actualizar")
     public void actualizar(PersonaTo persona) {
         this.personaService.actualizar(persona);
     }
 
-    public void borrar(Integer id) {
-        this.personaService.borrar(id);
+    @PATCH
+    @Path("/actualizarParcial")
+    public void actualizarParcial(PersonaTo persona) {
+        PersonaTo tmp = this.personaService.buscarPorId(persona.getId());
+        tmp.setNombre(persona.getNombre());
+        this.personaService.actualizar(tmp);
     }
 
-    @Path("/buscar")
-    @GET
-    public Response buscarPorId() {
+    @DELETE
+    @Path("/borrar")
+    public void borrar() {
         Integer id = 1;
-        //return this.personaService.buscarPorId(id);
-        return Response.ok(this.personaService.buscarPorId(id)).build();
+        this.personaService.borrar(id);
     }
 
 }
